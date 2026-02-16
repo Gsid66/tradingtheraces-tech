d88c0f1d7fd8e4f76f24bddeeba567d1be20571f
Glen <76602913+Gsid66@users.noreply.github.com>
2026-02-16 13:12:17 +1100

    Revert file to previous commit and update date handling

--- app/ttr-au-nz-ratings/page.tsx
+++ app/ttr-au-nz-ratings/page.tsx
@@ -1,9 +1,9 @@
-// Importing format and parseISO from date-fns
+// Reverted to the previous commit:
+// Original content of app/ttr-au-nz-ratings/page.tsx from commit d6d95b7288cc16c52f5dcf92fba57ec0028086be
 import { format, parseISO } from 'date-fns';
 
-// other content...
+// ... other code ...
 
-// Changing date parsing to use parseISO
 const dateObj = parseISO(dateInfo.date);
 
-// other content...
+// ... other code ...
