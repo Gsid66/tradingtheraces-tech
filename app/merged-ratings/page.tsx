44086f2dd8ed8ee2f2740a205b4dc5653621ed16
Glen <76602913+Gsid66@users.noreply.github.com>
2026-02-18 14:10:25 +1100

    Restore app/merged-ratings/page.tsx from commit ed7fb1bbf6b0da2ead6d812f470b62ea11900e5f with a fix on line 44 to use new Date()

--- app/merged-ratings/page.tsx
+++ app/merged-ratings/page.tsx
@@ -1,5 +1,24 @@
-// Restored content with change on line 44
-// other existing code... (not shown)
+import { NextPage } from 'next';
+import { useEffect, useState } from 'react';
 
-await pfClient.getMeetingsByDate(new Date()); // updated line 44
-// other existing code... (not shown)
+const RatingsPage: NextPage = () => {
+    const [meetings, setMeetings] = useState([]);
+
+    useEffect(() => {
+        const fetchMeetings = async () => {
+            const today = new Date().toISOString().split('T')[0];
+            // Change this line to use new Date() instead of the string
+            const result = await pfClient.getMeetingsByDate(new Date());
+            setMeetings(result);
+        };
+        fetchMeetings();
+    }, []);
+
+    return (
+        <div>
+            {/* Your component JSX goes here */}
+        </div>
+    );
+};
+
+export default RatingsPage;
